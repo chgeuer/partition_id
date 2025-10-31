05af58e5727ddc97b24e5937cd02330cce0b45c8
Christian Geuer-Pollmann <christian@geuer-pollmann.de>
2025-10-31 17:47:06 +0100

    Empty skeleton

---
+++ elixir/lib/azure_partition_id.ex
@@ -0,0 +1,18 @@
+defmodule AzurePartitionId do
+  @moduledoc """
+  Documentation for `AzurePartitionId`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AzurePartitionId.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/test/azure_partition_id_test.exs
@@ -0,0 +1,8 @@
+defmodule AzurePartitionIdTest do
+  use ExUnit.Case
+  doctest AzurePartitionId
+
+  test "greets the world" do
+    assert AzurePartitionId.hello() == :world
+  end
+end
